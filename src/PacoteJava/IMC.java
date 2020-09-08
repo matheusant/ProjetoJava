4f4f9b85e03763f351267506c860bf8370b3d413
Matheus Santos <matheusantos99@gmail.com>
2020-09-08 17:35:27 -0300

    Exercicio2

--- src/PacoteJava/IMC.java
+++ src/PacoteJava/IMC.java
@@ -6,17 +6,20 @@ public class IMC {
     public static void main(String[] args) {
         double num1, num2, imc=0;
         String aux ="";
+    
         
-        aux = JOptionPane.showInputDialog(null, "Entre com seu peso: ");
+        aux = JOptionPane.showInputDialog(null, "Entre com seu peso: ", "Peso", JOptionPane.QUESTION_MESSAGE);
+        aux = aux.replace(',', '.');
         num1 = Double.parseDouble(aux);
         
         if(num1<0.5){
-            JOptionPane.showMessageDialog(null, "Valor Incorreto");
+            JOptionPane.showMessageDialog(null, "Valor Incorreto", "Aviso", JOptionPane.ERROR_MESSAGE);
             System.exit(0);
             
         }
         
-        aux = JOptionPane.showInputDialog(null, "Entre com sua altura: ");
+        aux = JOptionPane.showInputDialog(null, "Entre com sua altura: ", "Altura", JOptionPane.QUESTION_MESSAGE);
+        aux = aux.replace(',', '.');
         num2 = Double.parseDouble(aux);
         
         if(num2>0.1){
@@ -26,7 +29,7 @@ public class IMC {
            
             
         }else{
-            JOptionPane.showMessageDialog(null, "Valor Incorreto");
+            JOptionPane.showMessageDialog(null, "Valor Incorreto", "Aviso", JOptionPane.ERROR_MESSAGE);
             System.exit(0);
         }
         
@@ -35,32 +38,32 @@ public class IMC {
         
                 
         if(imc<18.5){
-            JOptionPane.showMessageDialog(null, "Classificação: Excesso de Magreza");
+            JOptionPane.showMessageDialog(null, "Classificação: Excesso de Magreza", "RESULTADO", JOptionPane.INFORMATION_MESSAGE);
             
         }
         if(imc>=18.5 && imc<25){
     
-            JOptionPane.showMessageDialog(null, "Classificação: Peso Normal");
+            JOptionPane.showMessageDialog(null, "Classificação: Peso Normal", "RESULTADO", JOptionPane.INFORMATION_MESSAGE);
         }
         
         if(imc>=25 && imc<30){
     
-            JOptionPane.showMessageDialog(null, "Classificação: Excesso de Peso");
+            JOptionPane.showMessageDialog(null, "Classificação: Excesso de Peso", "RESULTADO", JOptionPane.INFORMATION_MESSAGE);
         }
         
         if(imc>=30 && imc<35){
     
-            JOptionPane.showMessageDialog(null, "Classificação: Obesidade (Grau I)");
+            JOptionPane.showMessageDialog(null, "Classificação: Obesidade (Grau I)", "RESULTADO", JOptionPane.INFORMATION_MESSAGE);
         }
         
         if(imc>=35 && imc<=40){
     
-            JOptionPane.showMessageDialog(null, "Classificação: Obesidade (Grau II)");
+            JOptionPane.showMessageDialog(null, "Classificação: Obesidade (Grau II)", "RESULTADO", JOptionPane.INFORMATION_MESSAGE);
         }
         
         if(imc>40){
     
-            JOptionPane.showMessageDialog(null, "Classificação: Obesidade (Grau III)");
+            JOptionPane.showMessageDialog(null, "Classificação: Obesidade (Grau III)", "RESULTADO", JOptionPane.INFORMATION_MESSAGE);
         }
         
     }
